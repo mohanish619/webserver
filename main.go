8b8d8841563b20e84a9af9f87e43e4aec2617d14
mohanish619 <msd7276@gmail.com>
2020-12-01 12:04:52 +0530

    this my first program

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func main(){
+	r :=mux.NewRouter()
+	r.HandleFunc("/",handler).Methods("GET")
+	http.ListenAndServe(":8080",r)
+}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w,"Hello World!")
+}
